0432c6a0bd136de575fc68874fd24db77de1d821
Filipe Zaidan <felipezaidan10@gmail.com>
2024-10-03 23:03:54 -0300

    feat: :sparkles: login page

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from '@tanstack/react-router';
 export const AuthLayout = () => {
   return (
     <div className='flex h-screen w-screen bg-[#D7DDE5] p-4'>
-      <div className='flex-1 w-full max-w-xl mx-auto my-auto bg-white rounded-lg shadow-md h-3/5 overflow-hidden p-10'>
+      <div className='flex-1 w-full max-w-xl mx-auto my-auto bg-white rounded-xl shadow-md h-3/5 overflow-hidden p-10'>
         <Outlet />
       </div>
     </div>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,102 @@
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormMessage,
+  FormField,
+  FormItem,
+  FormControl,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { formLoginSchema } from '@/schema/Login';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Link } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 export const LoginPage = () => {
-  return (
-    <div>
-      <h1 className='text-2xl font-bold'>SIGHAS</h1>
-      <h2 className='text-lg font-medium'>Login</h2>
+  const form = useForm<z.infer<typeof formLoginSchema>>({
+    resolver: zodResolver(formLoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
 
-      <div>
-        <p>Novo no SIGHAS?</p>
-        <Link to='/register'>Registre-se gratuitamente</Link>
+  const onSubmit = (data: z.infer<typeof formLoginSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <div className='flex flex-col flex-1 gap-8 items-center justify-center'>
+      <div className='flex flex-col gap-4 items-center justify-center'>
+        <h1 className='text-6xl font-bold text-[#172554]'>SIGHAS</h1>
+        <h2 className='text-5xl font-medium text-zinc-800 '>Login</h2>
+
+        <div className='flex  items-center gap-2'>
+          <p className=' text-zinc-500 text-base'>Novo no SIGHAS?</p>
+          <Link to='/register' className='text-[#172554] underline text-base'>
+            Registre-se gratuitamente
+          </Link>
+        </div>
       </div>
+
+      <Form {...form}>
+        <form
+          className='items flex flex-col gap-4 w-full'
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <Label>Email</Label>
+                <FormControl>
+                  {/* <Label>Email</Label> */}
+                  <Input
+                    placeholder='Digita seu email'
+                    type='email'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <Label>Senha</Label>
+                <FormControl>
+                  {/* <Label>Senha</Label> */}
+                  {/* TODO: Add password lock icon and a show/hide password button */}
+                  <Input
+                    placeholder='Digita sua senha'
+                    type='password'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            color='#172554'
+            size='lg'
+            className='text-white bg-[#172554] hover:bg-[#172554]/90'
+            type='submit'
+            disabled={Object.keys(form.formState.errors).length > 0}
+          >
+            Login
+          </Button>
+          <div className='flex w-full items-center justify-center'></div>
+        </form>
+      </Form>
     </div>
   );
 };
