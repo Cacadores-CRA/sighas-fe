b3a9254448cb4e858de55ba3e98a1f24cafecd21
Filipe Zaidan <felipezaidan10@gmail.com>
2024-10-03 22:03:11 -0300

    feat: :sparkles: add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import { InnerApp } from './app';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <InnerApp />
+    <QueryClientProvider client={queryClient}>
+      <InnerApp />
+    </QueryClientProvider>
   </StrictMode>
 );
