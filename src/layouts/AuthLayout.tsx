0b74ad468701134e8e40da3fb71be1bf6adf604b
Filipe Zaidan <felipezaidan10@gmail.com>
2024-10-08 00:39:43 -0300

    style: :art: fix styles login page

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from '@tanstack/react-router';
 
 export const AuthLayout = () => {
   return (
-    <div className='flex h-screen w-screen bg-[#D7DDE5] p-4'>
-      <div className='flex-1 w-full max-w-xl mx-auto my-auto bg-white rounded-xl shadow-md h-3/5 overflow-hidden p-10'>
+    <div className='flex h-screen w-screen bg-[#D7DDE5] p-4 shadow-md'>
+      <div className='flex-1 w-full max-w-96 mx-auto my-auto bg-white rounded-xl shadow-md max-h-[500px] overflow-hidden p-8'>
         <Outlet />
       </div>
     </div>
