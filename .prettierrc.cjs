29a62a6d38fdc5547498fd2a5d84210b243161c9
Lucas Matheus <lucmvl2003@gmail.com>
2024-12-06 16:22:29 -0300

    fix: auto-formatting

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,3 @@
-// @ts-check
-
-// /** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 /** @type {import("prettier").Config} */
 const config = {
   endOfLine: 'lf',
@@ -10,36 +7,36 @@ const config = {
   trailingComma: 'es5',
   useTabs: false,
   tabWidth: 2,
-  // importOrder: [
-  //   '^(react/(.*)$)|^(react$)',
-  //   '<BUILTIN_MODULES>',
-  //   '<THIRD_PARTY_MODULES>',
-  //   '',
-  //   '^types$',
-  //   '^@/types/(.*)$',
-  //   '^@/config/(.*)$',
-  //   '^@/lib/(.*)$',
-  //   '^@/hooks/(.*)$',
-  //   '^@/components/ui/(.*)$',
-  //   '^@/components/(.*)$',
-  //   '^@/schemas/(.*)$',
-  //   '^@/mocks/(.*)$',
-  //   '',
-  //   '^@/assets/(.*)$',
-  //   '^@/icons/(.*)$',
-  //   '',
-  //   '^@/layouts/(.*)$',
-  //   '^@/routes/(.*)$',
-  //   '^@/pages/(.*)$',
-  //   '',
-  //   '^[./]',
-  // ],
-  // importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
-  // importOrderTypeScriptVersion: '5.3.3',
-  plugins: [
-    // 'prettier-plugin-tailwindcss',
-    // '@ianvs/prettier-plugin-sort-imports',
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '<BUILTIN_MODULES>',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/schemas/(.*)$',
+    '^@/mocks/(.*)$',
+    '',
+    '^@/assets/(.*)$',
+    '^@/icons/(.*)$',
+    '',
+    '^@/layouts/(.*)$',
+    '^@/routes/(.*)$',
+    '^@/pages/(.*)$',
+    '',
+    '^[./]',
   ],
-};
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.3.3',
+  plugins: [
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports',
+  ],
+}
 
-module.exports = config;
+module.exports = config
